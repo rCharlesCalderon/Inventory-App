930d509064d8b274fdf3aaf5ccbc1e5a3bc2bc47
Ruben Calderon <Rcaldeon0907@gmail.com>
2024-02-10 20:29:47 -0800

    .

--- my-ejs-project/app.js
+++ my-ejs-project/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use("/uploads", express.static("uploads"));
 app.use('/users', usersRouter);
 
 
--- my-ejs-project/routes/index.js
+++ my-ejs-project/routes/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const {ObjectId} = require("mongodb");
 const itemSchema = require("../controller/itemSchema.js")
+const multer = require("multer");
+const { func } = require("prop-types");
+const upload = multer({ dest: "uploads/" });
 
 mongoose.connect("mongodb+srv://Rubcal123:Rubcal123@inventory.smc01ik.mongodb.net/?retryWrites=true&w=majority");
 const db = mongoose.connection.useDb("Categories")
@@ -17,6 +20,7 @@ router.get("/", function (req, res, next) {
 //Lists Categories
 router.get("/categories", async function (req, res) {
   const collectionData = await db.listCollections();
+
   res.render("categories", { collections: collectionData});
 });
 
@@ -31,10 +35,13 @@ router.post("/createCategories", async function (req, res) {
   const categoryData = await db.listCollections();
   let findData = categoryData.find((name) => name.name === categoryName);
   if(!findData){
-  const category = await db.createCollection(categoryName);
+   await db.createCollection(categoryName);
   }
   res.redirect(`/categories/${categoryName}`)
 });
+
+
+
 
 //List all documents of a collection
 router.get("/categories/:id", async function (req, res) {
@@ -42,6 +49,35 @@ router.get("/categories/:id", async function (req, res) {
   const categoryData = await db.collection(id).find().toArray();
   res.render("category", { categoryData, collection: id });
 });
+
+router.post("/deleteCategory",async function(req,res){
+const collectionName = req.query.collection;
+await db.dropCollection(collectionName)
+res.redirect("/")
+})
+
+
+
+//Render Category Name Change form
+router.get("/updateCategory",async function(req,res){
+const collectionName = req.query.collection;
+  res.render("updateCategory",{collectionName});
+});
+
+
+//Change Category/Collection Name
+router.post("/updateCategoryName", async function (req, res) {
+  const collectionName = req.query.collectionName;
+  let test = req.body.updateCategoryInput;
+  await db.collection(collectionName).rename(test)
+  res.redirect('/')
+});
+
+
+
+
+
+
 
 //Lists all items in Items tab
 router.get("/items", async function(req,res){
@@ -76,14 +112,14 @@ router.get('/items/create',async function(req,res){
   res.render("createItem",{categories})
 })
 //Store item in MongoDB
-router.post("/createItem",async function(req,res){
+router.post("/createItem", upload.single('itemImage'),async function(req,res){
   //CREATE SCHEMA OR USE IT HERE 
  const newItem = db.model(req.body.categoryNames, itemSchema,req.body.categoryNames);
  const newItemInstance = new newItem({
    name: req.body.itemName,
    description: req.body.itemDescription,
    category: req.body.categoryNames,
-   image: req.body.image,
+   image: req.file.filename,
    price: req.body.price,
    stock: req.body.stock,
  });
